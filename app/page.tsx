0758df332c6ed6420e9fc99bb0939bf441847128
success20242 <ebosisuccess@gmail.com>
2025-06-25 16:58:34 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,10 @@ import TrendingVideo from "../trending-video"
 import AffiliateSidebar from "@/components/affiliate-sidebar"
 
 export default function Page() {
-  // Updated video title
-  const videoTitle = "Welcome to TrendifyTube"
-  const videoTags = ["Brawl Stars", "Starr Park"]
-
   return (
     <div style={{ display: "flex" }}>
       <div style={{ width: 320, flexShrink: 0, borderRight: "1px solid #eee", minHeight: "100vh" }}>
-        <AffiliateSidebar videoTitle={videoTitle} videoTags={videoTags} />
+        <AffiliateSidebar />
       </div>
       <div style={{ flex: 1 }}>
         <TrendingVideo />
