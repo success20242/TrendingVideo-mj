bd0841e15c1ec202dad24e97f952dabc22ad3c95
success20242 <ebosisuccess@gmail.com>
2025-06-27 09:06:04 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
@@ -14,6 +15,21 @@ export default function MyApp({ Component, pageProps }) {
         {/* AdSense meta tag */}
         <meta name="google-adsense-account" content="ca-pub-2261833870173099" />
       </Head>
+      {/* Google Analytics 4 */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-L6CN6PWBZ0`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-L6CN6PWBZ0', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
