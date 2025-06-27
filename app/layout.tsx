762ceac0191dfe02017ce2be364d822537354966
success20242 <ebosisuccess@gmail.com>
2025-06-27 16:39:59 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,30 @@ export const metadata: Metadata = {
   title: "TrendifyTube",
   description: "Watch trending videos from around the world",
   generator: "v0.dev",
+  openGraph: {
+    title: "TrendifyTube",
+    description: "Watch trending videos from around the world",
+    url: "https://trendifyhub.vercel.app",
+    siteName: "TrendifyTube",
+    images: [
+      {
+        url: "https://i.ibb.co/wZzWzBpJ/Colorful-Minimalist-Social-Community-Logo-removebg-preview.png",
+        width: 1200,
+        height: 630,
+        alt: "TrendifyTube Logo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "TrendifyTube",
+    description: "Watch trending videos from around the world",
+    images: [
+      "https://i.ibb.co/wZzWzBpJ/Colorful-Minimalist-Social-Community-Logo-removebg-preview.png",
+    ],
+  },
 }
 
 export default function RootLayout({
